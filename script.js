4a08b0b10b1f6af119ed3fb963a41438d8e3dee1
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-01-29 14:23:41 -0600

    task renders when page refreshed

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-var currentHour = moment().minutes();
-console.log(currentHour);
+var body = document.body;
 
-var a = moment().format();
-console.log(a);
+
+var a = moment().format('dddd, MMMM Do');
 
 var saveBtn = $(".saveBtn");
 var currentDay = $("#currentDay");
 currentDay.text(a);
+var hour = $('.hour');
+var description = $('.description');
+console.log(description)
+
+
+
+console.log(moment().fromNow());
+console.log(moment().hour())
+hourColor();
+
+renderTask();
+
+
+function hourColor() {
+    if (hour.val() !== moment().hour()) {
+        description.attr('class', 'future col-md-10')
+    }
+
+    if (hour.val() == moment().hour()) {
+        description.attr('class', 'present')
+    }
+}
+
+
+function renderTask() {
+    var task = localStorage.getItem('task')
+    description.text(task);
+
+};
+
+saveBtn.on('click', function () {
+    event.preventDefault();
+
+    var task = description.val();
+    console.log(task)
+
+    localStorage.setItem('task', task);
+    renderTask();
+});
