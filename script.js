04deeaa7a2542ef3802096a893da73ff00cbe8b9
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-01-30 12:58:35 -0600

    each save button saves individual hour task

--- script.js
+++ script.js
@@ -11,36 +11,39 @@ $(document).ready(function () {
     
     currentDay.text(today);
     hourColor();
-    renderTask();
 
     function hourColor() {
         description.each(function () {
             var rowHour = $(this).attr('id');
             if (rowHour < currentHour) {
-                $(this).attr('class', 'past col-md-10');
+                $(this).attr('class', 'past col-md-10 description');
             }
             else if (rowHour > currentHour) {
-                $(this).attr('class', 'future col-md-10')
+                $(this).attr('class', 'future col-md-10 description')
             }
             else {
-                $(this).attr('class', 'present col-md-10')
+                $(this).attr('class', 'present col-md-10 description')
             };
         });
     };
 
-    function renderTask() {
-        var task = localStorage.getItem('task')
-        description.text(task);
-    };
-
     saveBtn.on('click', function () {
 
-        // event.preventDefault();
-        var task = description.val();
+        var task = $(this).siblings('.description').val();
+        var time = $(this).siblings('.description').attr('id');
         console.log(task)
 
-        localStorage.setItem('task', task);
-        renderTask();
+        localStorage.setItem(time, task);
     });
+
+    $('#9').text(localStorage.getItem('9'));
+    $('#10').text(localStorage.getItem('10'));
+    $('#11').text(localStorage.getItem('11'));
+    $('#12').text(localStorage.getItem('12'));
+    $('#13').text(localStorage.getItem('13'));
+    $('#14').text(localStorage.getItem('14'));
+    $('#15').text(localStorage.getItem('15'));
+    $('#16').text(localStorage.getItem('16'));
+    $('#17').text(localStorage.getItem('17'));
 
 });
