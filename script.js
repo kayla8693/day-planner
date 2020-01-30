4928544e964b58288cd11da1f53ab1b2be09606b
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-01-29 22:29:51 -0600

    past/present/future colors set

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-var body = document.body;
+$(document).ready(function () {
 
+    var today = moment().format('dddd, MMMM Do');
+    var saveBtn = $(".saveBtn");
+    var currentDay = $("#currentDay");
+    var currentHour = moment().hour();
+    var hour = $('.hour');
+    var description = $('.description');
 
-var a = moment().format('dddd, MMMM Do');
-
-var saveBtn = $(".saveBtn");
-var currentDay = $("#currentDay");
-currentDay.text(a);
-var hour = $('.hour');
-var description = $('.description');
-console.log(description)
-
-
-
-console.log(moment().fromNow());
-console.log(moment().hour())
-hourColor();
-
-renderTask();
-
-
-function hourColor() {
-    if (hour.val() !== moment().hour()) {
-        description.attr('class', 'future col-md-10')
-    }
-
-    if (hour.val() == moment().hour()) {
-        description.attr('class', 'present')
-    }
-}
-
-
-function renderTask() {
-    var task = localStorage.getItem('task')
-    description.text(task);
-
-};
-
-saveBtn.on('click', function () {
-    event.preventDefault();
-
-    var task = description.val();
-    console.log(task)
-
-    localStorage.setItem('task', task);
+    console.log(currentHour)
+    
+    currentDay.text(today);
+    hourColor();
     renderTask();
+
+    function hourColor() {
+        description.each(function () {
+            var rowHour = $(this).attr('id');
+            if (rowHour < currentHour) {
+                $(this).attr('class', 'past col-md-10');
+            }
+            else if (rowHour > currentHour) {
+                $(this).attr('class', 'future col-md-10')
+            }
+            else {
+                $(this).attr('class', 'present col-md-10')
+            };
+        });
+    };
+
+    function renderTask() {
+        var task = localStorage.getItem('task')
+        description.text(task);
+    };
+
+    saveBtn.on('click', function () {
+
+        // event.preventDefault();
+        var task = description.val();
+        console.log(task)
+
+        localStorage.setItem('task', task);
+        renderTask();
+    });
+
 });
