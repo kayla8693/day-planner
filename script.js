3c3c2513394225f2e81234549ae918e1abb52b6d
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-01-30 14:16:28 -0600

    added comments

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
-
+    // moment.js used to get today's date and current hour
     var today = moment().format('dddd, MMMM Do YYYY');
     var saveBtn = $(".saveBtn");
     var currentDay = $("#currentDay");
     var currentHour = moment().hour();
-    var hour = $('.hour');
     var description = $('.description');
 
     console.log(currentHour)
     
+// gets todays date from variable
     currentDay.text(today);
+// calls function to check rowtime + change color based on that
     hourColor();
 
     function hourColor() {
+// for each element with .description...
         description.each(function () {
+
+// rowHour = the id of each element, which is the element's hour #
             var rowHour = $(this).attr('id');
+
+// attach all classes back to the element
             if (rowHour < currentHour) {
                 $(this).attr('class', 'past col-md-10 description');
             }
@@ -27,15 +33,18 @@ $(document).ready(function () {
         });
     };
 
-    saveBtn.on('click', function () {
 
+    saveBtn.on('click', function () {
+// task = any sibling of this button w .description's value, which = user input
         var task = $(this).siblings('.description').val();
+// time = any sibling of this button w .description's id, which is the hour
         var time = $(this).siblings('.description').attr('id');
         console.log(task)
 
         localStorage.setItem(time, task);
     });
 
+// gets task from local storage + sets it to text of its appropriate hour
     $('#9').text(localStorage.getItem('9'));
     $('#10').text(localStorage.getItem('10'));
     $('#11').text(localStorage.getItem('11'));
